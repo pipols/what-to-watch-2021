85743819c792253218be7113669e37114185b423
pipols <pipols2007@gmail.com>
2021-03-14 23:24:29 +0300

    черновой вариант review

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
+import {CommentType} from "../../types";
 
-const Reaview = () => {
+type Props = {
+  comment: CommentType,
+};
+
+const Reaview = ({comment}: Props) => {
   return (
     <div className="review">
       <blockquote className="review__quote">
-        <p className="review__text">Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+        <p className="review__text">{comment.comment}</p>
 
         <footer className="review__details">
-          <cite className="review__author">Bill Goodykoontz</cite>
-          <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+          <cite className="review__author">{comment.user.name}</cite>
+          <time className="review__date" dateTime="2015-11-18">{comment.date}</time>
         </footer>
       </blockquote>
 
-      <div className="review__rating">8,0</div>
+      <div className="review__rating">{comment.rating}</div>
     </div>
   );
 };
