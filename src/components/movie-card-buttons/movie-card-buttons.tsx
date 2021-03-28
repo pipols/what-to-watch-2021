15625f268c0268c84695b2030190aa619fccfffa
pipols <pipols2007@gmail.com>
2021-03-28 22:00:35 +0300

    component MovieCardButtons

---
+++ src/components/movie-card-buttons/movie-card-buttons.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+const MovieCardButtons = () => {
+  return (
+    <div className="movie-card__buttons">
+      <button className="btn btn--play movie-card__button" type="button">
+        <svg viewBox="0 0 19 19" width="19" height="19">
+          <use xlinkHref="#play-s"></use>
+        </svg>
+        <span>Play</span>
+      </button>
+      <button className="btn btn--list movie-card__button" type="button">
+        <svg viewBox="0 0 19 20" width="19" height="20">
+          <use xlinkHref="#add"></use>
+        </svg>
+        <span>My list</span>
+      </button>
+      <a href="add-review.html" className="btn movie-card__button">Add review</a>
+    </div>
+  );
+};
+
+export default MovieCardButtons;
+
