67e1797878514c55eb1f348355d6230967d3fa71
pipols <pipols2007@gmail.com>
2021-04-05 09:58:49 +0300

    privatRouter

---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {Route, Redirect, RouteProps} from "react-router-dom";
+import {connect} from "react-redux";
+import {AuthorizationStatus} from "../../const/common";
+import {getAuthorizationStatus} from "../../reducer/user/selector.js";
+
+type Props = RouteProps & {
+  authorizationStatus: string;
+  render: () => React.ReactNode;
+}
+
+const PrivateRoute = (props: Props) => {
+  const {render, path, exact, authorizationStatus} = props;
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={() => {
+        return (
+          authorizationStatus === AuthorizationStatus.AUTH
+            ? render()
+            : <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: getAuthorizationStatus(state),
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
